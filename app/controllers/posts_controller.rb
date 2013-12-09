41cbd139aa5ca540319222104c09bd654d976955
Nicola <nikifiona@gmail.com>
2013-12-09 00:57:08 -0500

    Second Commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,41 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
 
+  end
+
+  def show
+    
+    @post = Post.find(params[:id])
+  end
+
+
+  def new
+    @post = Post.new
+    
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
   end
 end
